c74833663826abcf07b4aa5dd3f82b3d9221e353
Sina-Lotfi <sinalotfi531@gmail.com>
2023-01-30 02:03:45 +0330

    just push

--- databse-project/dashboard.py
+++ databse-project/dashboard.py
@@ -4,6 +4,10 @@ from employee import employeeClass
 from supplier import supplierClass
 from category import categoryClass
 from product import productClass
+from tkinter import messagebox
+import sqlite3 
+import time
+import os
 class WMS:
     def __init__(self, root) -> None:
         self.root = root
@@ -17,7 +21,7 @@ class WMS:
         title = Label(self.root, text="سیستم مدیریت انبارداری", font=("times", 40, "bold"), image=self.icon_title,
                       compound=RIGHT, bg="#010c48", fg="white", anchor="e", padx=20).place(x=0, y=0, relwidth=1, height=70)
         # ==========btn_logout======
-        btn_logout = Button(self.root, text="خروج", font=(
+        btn_logout = Button(self.root, text="خروج",command=self.logout, font=(
             "times", 15, "bold"), bg="yellow", cursor="hand2").place(x=10, y=10, height=50, width=150)
         # ==========clock===========
         self.lbl_clock = Label(self.root, text="تاریخ: DD-MM-YYYY\t\t زمان: HH:MM:SS\t\t\t\t\t به سیستم مدیریت انبارداری خودش امدید ",
@@ -43,9 +47,9 @@ class WMS:
             "times", 20, "bold"), bg="white", bd=2, cursor="hand2").pack(side=TOP, fill=X)
         lbl_product = Button(LeftMenu, text="محصولات",command=self.product, image=self.icon_side, compound=RIGHT, padx=20, anchor="e", font=(
             "times", 20, "bold"), bg="white", bd=2, cursor="hand2").pack(side=TOP, fill=X)
-        lbl_sale = Button(LeftMenu, text="فروش",image=self.icon_side, compound=RIGHT,padx=20, anchor="e", font=(
+        lbl_sale = Button(LeftMenu, text="دکمه رو نزن",command=self.nothing,image=self.icon_side, compound=RIGHT,padx=20, anchor="e", font=(
             "times", 20, "bold"), bg="white", bd=2, cursor="hand2").pack(side=TOP, fill=X)
-        lbl_exit = Button(LeftMenu, text="خروج", image=self.icon_side, compound=RIGHT, padx=20, anchor="e", font=(
+        lbl_exit = Button(LeftMenu, text="خروج",command=self.logout, image=self.icon_side, compound=RIGHT, padx=20, anchor="e", font=(
             "times", 20, "bold"), bg="white", bd=2, cursor="hand2").pack(side=TOP, fill=X)
         #===========content=============
         self.lbl_employee = Label(self.root, text="تعداد کارمندان\n[ 0 ]", bd=5, relief=RIDGE, bg="#33bbf9", fg="white", font=(
@@ -65,6 +69,7 @@ class WMS:
         self.lbl_sales.place(x=360, y=280, height=150, width=300)
         #===========Footer==============
         lbl_footer = Label(self.root, text="سیستم مدیریت انبارداری | توسعه داده شده توسط تیم هشتگ \n دانشگاه صنعتی همدان ترم پاییز ۴۰۱",font=("times", 10), bg="#4d636d", fg="white").pack(side=BOTTOM, fill=X)
+        self.update_content()
         #==============================================================================================
     def employee(self):
         self.new_win = Toplevel(self.root)
@@ -81,6 +86,44 @@ class WMS:
     def product(self):
         self.new_win = Toplevel(self.root)
         self.new_obj = productClass(self.new_win)
+    def logout(self):
+        self.root.destroy()
+        os.system("python login.py")
+    def nothing(self):
+        self.forget_win = Toplevel(self.root)
+        self.forget_win.title("هیچی")
+        self.forget_win.geometry("400x350+500+100")
+        self.forget_win.focus_force()
+        title = Label(self.forget_win, text="داداش مگه نگفتم نزن\nچرا زدی", font=(
+            "goudy old style", 40, "bold"), bg=("red"), fg="white").pack(side=TOP, fill=X)
+    def update_content(self):
+        con = sqlite3.connect(database=r'wms.db')
+        cur = con.cursor()
+        try:
+            cur.execute("select * from category")
+            category = cur.fetchall()
+            self.lbl_category.config(
+                text=f"تعداد دسته‌بندی\n[ {str(len(category))} ]")
+            cur.execute("select * from employee")
+            employee = cur.fetchall()
+            self.lbl_employee.config(
+                text=f"تعداد کارمندان\n[ {str(len(employee))} ]")
+            cur.execute("select * from category")
+            supplier = cur.fetchall()
+            self.lbl_supliers.config(
+                text=f"تعداد منابع\n[ {str(len(supplier))} ]")
+            cur.execute("select * from category")
+            product = cur.fetchall()
+            self.lbl_products.config(
+                text=f"تعداد محصولات\n[ {str(len(product))} ]")
+            time_ = time.strftime("%I:%M:%S")
+            date_ = time.strftime("%d-%m-%Y")
+            self.lbl_clock.config(text=f"تاریخ: {date_}\t\t زمان: {time_}\t\t\t\t\t به سیستم مدیریت انبارداری خودش امدید ")
+            self.lbl_clock.after(200,self.update_content)
+        except Exception as ex:
+            messagebox.showerror(
+                "خطا", f"خطای رخ داده : {str(ex)}", parent=self.root)
+        
 if __name__ == "__main__":
     root = Tk()
     obj = WMS(root)
---
+++ databse-project/login.py
@@ -0,0 +1,136 @@
+from tkinter import *
+from PIL import ImageTk
+from tkinter import messagebox
+import sqlite3
+import os
+class Login_System:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("صفحه ورود")
+        self.root.geometry("1350x700+0+0")
+        self.root.config(bg="#fafafa")
+        # ==== images==========
+        self.phone_image = PhotoImage(file="images\phone.png")
+        self.lbl_image_phone = Label(
+            self.root, image=self.phone_image, bd=0).place(x=200, y=50)
+        # =====login Frame============
+        login_frame = Frame(self.root, bd=2, relief=RIDGE, bg="white")
+        login_frame.place(x=650, y=90, width=350, height=460)
+
+        title = Label(login_frame, text="فرم ورود", font=(
+            "Elephant", 30, "bold"), bg="white").place(x=0, y=30, relwidth=1)
+
+        lbl_user = Label(login_frame, text="کد پرسنلی", font=(
+            "Elephant", 15), bg="white", fg="#767171").place(x=50, y=100)
+        self.employee_id = StringVar()
+        self.password = StringVar()
+        tex_employee_id = Entry(login_frame, textvariable=self.employee_id, font=(
+            "times", 15), bg="#ECECEC").place(x=50, y=140, width=250)
+
+        lbl_pass = Label(login_frame, text="رمز ورود", font=(
+            "Elephant", 15), bg="white", fg="#767171").place(x=50, y=190)
+        tex_password = Entry(login_frame, textvariable=self.password, show="*", font=(
+            "times", 15), bg="#ECECEC").place(x=50, y=240, width=250)
+
+        btn_login = Button(login_frame, text="ورود", command=self.login, font=("Arial Rounded MT Bold", 15),
+                           bg="#00B0F0", activebackground="#00B0F0", fg="white", activeforeground="white", cursor="hand2").place(x=50, y=300, width=250, height=35)
+        hr = Label(login_frame, bg="lightgray").place(
+            x=50, y=370, width=250, height=2)
+        or_ = Label(login_frame, text="یا", fg="lightgray",
+                    bg="white", font=("times", 15, "bold")).place(x=150, y=355)
+
+        #btn_forget = Button(login_frame, text="slam",command=self.login, font=("times", 13), bg="white",
+                            #fg="#00759E", bd=0, activebackground="white", activeforeground="#00759E").place(x=100, y=390)
+        # ========= frame =============
+        register_frame = Frame(self.root, bd=2, relief=RIDGE, bg="white")
+        register_frame.place(x=650, y=570, width=350, height=60)
+        lbl_reg = Label(register_frame, text="سیستم مدیریت انبارداری سپهر", font=(
+            "times", 13), bg="white").place(x=80, y=20)
+        btn_forget = Button(login_frame, text="رمز خود  را فراموش کردید؟",command=self.forget, font=("times", 13), bg="white",
+                            fg="#00759E", bd=0, activebackground="white", activeforeground="#00759E").place(x=100, y=390)
+        # ======= animations images================
+        self.im1 = ImageTk.PhotoImage(file="images/im1.png")
+        self.im2 = ImageTk.PhotoImage(file="images/im2.png")
+        self.im3 = ImageTk.PhotoImage(file="images/im3.png")
+
+        self.lbl_change_image = Label(self.root, bg="white")
+        self.lbl_change_image.place(x=367, y=153, width=240, height=428)
+
+        self.animated()
+        #======== All functions ===============
+    def animated(self):
+        self.im = self.im1
+        self.im1 = self.im2
+        self.im2 = self.im3
+        self.im3 = self.im
+        self.lbl_change_image.config(image=self.im)
+        self.lbl_change_image.after(2000, self.animated)
+
+    def login(self):
+        con = sqlite3.connect(database=r'wms.db')
+        cur = con.cursor()
+        try:
+            if self.employee_id.get() == "" or self.password.get()=="":
+                messagebox.showerror("خطا","تمامی فیلد هارا پر کنید",parent=self.root)
+            else:
+                cur.execute("Select utype from employee where eid=? AND pass=?",(self.employee_id.get(),self.password.get()))
+                user = cur.fetchone()
+                if user == None:
+                    messagebox.showerror(
+                        "خطا", "نام کاربری یا رمز ورود اشتباه است دوباره امتحان کنید", parent=self.root)
+                else:
+                    if user == "مدیر":
+                        self.root.destroy()
+                        os.system("Python dashboard.py")
+        except Exception as ex:
+            messagebox.showerror(
+                "خطا", f"خطای رخ داده : {str(ex)}", parent=self.root)
+
+    def forget(self):
+        con = sqlite3.connect(database=r'wms.db')
+        cur = con.cursor()
+        try:
+            if self.employee_id.get() == "":
+                messagebox.showerror("خطا", "کد پرسنلی را وارد کنید",parent=self.root)
+            else:
+                cur.execute("Select email from employee where eid=?",
+                            (self.employee_id.get(),))
+                email = cur.fetchone()
+                if email == None:
+                    messagebox.showerror(
+                        "خطا", "کد پرسنلی اشتباه است دوباره امتحان کنید", parent=self.root)
+                else:
+                    self.var_otp = StringVar()
+                    self.var_new_pass = StringVar()
+                    self.var_conf_pass = StringVar()
+                    #===== forget window ==========
+                    #call send_email() function
+                    self.forget_win = Toplevel(self.root)
+                    self.forget_win.title("بازیابی رمز ورود")
+                    self.forget_win.geometry("400x350+500+100")
+                    self.forget_win.focus_force()
+                    title = Label(self.forget_win, text="بازیابی رمز ورود", font=("goudy old style", 15, "bold"), bg=("#3f51b5"), fg="white").pack(side=TOP, fill=X)
+                    lbl_reset = Label(self.forget_win, text="ایمیل خود را برای بازیابی وارد کنید",font=("times", 15)).place(x=20,y=60)
+                    tex_reset = Entry(self.forget_win, textvariable=self.var_otp, font=("times", 15),bg="lightyellow").place(x=20, y=100,width=250, height=30)
+                    self.btn_reset = Button(self.forget_win, text="ثبت",font=("times", 15), bg="lightblue")
+                    self.btn_reset.place(x=280,y=100,width=100, height=30)
+                    lbl_new_pass = Label(self.forget_win, text="رمزعبور جدید را وارد نمایید", font=(
+                        "times", 15)).place(x=20, y=160)
+                    tex_new_pass = Entry(self.forget_win, textvariable=self.var_new_pass, font=(
+                        "times", 15), bg="lightyellow").place(x=20, y=195, width=250, height=30)
+                    lbl_c_pass = Label(self.forget_win, text="تایید رمزعبور", font=(
+                        "times", 15)).place(x=20, y=225)
+                    tex_c_pass = Entry(self.forget_win, textvariable=self.var_conf_pass, font=(
+                        "times", 15), bg="lightyellow").place(x=20, y=255, width=250, height=30)
+                    self.btn_update = Button(
+                        self.forget_win, text="بروزرسانی", font=("times", 15), bg="lightblue")
+                    self.btn_update.place(x=150, y=295, width=100, height=30)
+        except Exception as ex:
+            messagebox.showerror(
+                "خطا", f"خطای رخ داده : {str(ex)}", parent=self.root)
+
+
+
+root = Tk()
+obj = Login_System(root)
+root.mainloop()
